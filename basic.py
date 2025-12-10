d562cf29f94b4169ce70312214090e3edd646060
PriyaAdhikaritutorial <priyaaadhikari05@gmail.com>
2025-12-10 15:10:11 +0545

    update

---
+++ basic.py
@@ -0,0 +1,11 @@
+# print("Hello python")
+# print("It's a python code to say hello")
+a = int(20) 
+print(type(a))
+
+b = ["apple", "mango", "cherry"] 
+print(type(b))
+
+# a =  float(10.0)
+# print(type(a))
+
---
+++ list_example.py
@@ -0,0 +1,53 @@
+# thislist = ["pencil", "pen", "eraser"]
+# print(thislist[0])
+
+
+# thislist = ["apple", "banana", "orange"]
+# print(thislist[1])
+
+# thislist = ["priya", "asmita", "supriya"]
+# thislist[1] = "priyanssha"
+
+# print(thislist) 
+
+# thislist = ["iphone 15", "iphone 16", "iphone 17"]
+# thislist[0] = "iphone 15 pro"
+# print(thislist)
+
+# thislist = ['pen', 'pencil', 'eraser']
+# thislist.append('colors')
+# print(thislist) 
+
+# thislist = ["yellow","red", "pink"]
+# thislist.insert(1, "blue")
+# print(thislist)
+
+# thislist = ["apple", "banana", "orange"]
+# thislist.remove("apple")
+# print(thislist)
+
+# thislist = ["alex", "arden", "amike"]
+# thislist.pop(2)
+# print(thislist)
+
+# thislist = ["alex", "arden", "amike"]
+# thislist.pop()
+# print(thislist)
+
+# a = ["yellow", "pink", "red"]
+# del a[2]
+# print(a)
+
+# b = ["hello", "hi"]
+# del b [0]
+# print(b)
+
+# data = ["yellow", "red", "black"]
+# new=data.copy()
+# print(new)
+
+
+
+
+
+
