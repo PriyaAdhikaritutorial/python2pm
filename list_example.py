6f734a1ee8dbef8cec137367692d4a5f12bbcde2
PriyaAdhikaritutorial <priyaaadhikari05@gmail.com>
2025-12-16 15:13:37 +0545

    update list example file

--- list_example.py
+++ list_example.py
@@ -114,28 +114,52 @@
 #     print("login failed")
 
 
-print("=========================welcome to computer bazar")
-print("1. Dell(Rs:20000) 2. HP(Rs:25000) 3. Apple(Rs:50000) 4. Lenovo(Rs:30000)" )
-option = int(input("Enter quantity:"))
-if option==1:
-    qu= int(input("Enter quantity: "))
-    name= input("Enter your name: ")
-    total= 20000*qu
-    print(f"total amount:{total}")
-elif option==2:
-    qu= int(input("Enter quantity: "))
-    name= input("Enter your name: ")
-    total= 25000*qu
-    print(f"total amount:{total}")
-elif option==3:
-    qu= int(input("Enter quantity: "))
-    name= input("Enter your name: ")
-    total= 50000*qu
-    print(f"total amount:{total}")
-elif option==4:
-    qu= int(input("Enter quantity: "))
-    name= input("Enter your name: ")
-    total= 30000*qu
-    print(f"total amount:{total}")
+# print("=========================welcome to computer bazar")
+# print("1. Dell(Rs:20000) 2. HP(Rs:25000) 3. Apple(Rs:50000) 4. Lenovo(Rs:30000)" )
+# option = int(input("Enter quantity:"))
+# if option==1:
+#     qu= int(input("Enter quantity: "))
+#     name= input("Enter your name: ")
+#     total= 20000*qu
+#     print(f"total amount:{total}")
+# elif option==2:
+#     qu= int(input("Enter quantity: "))
+#     name= input("Enter your name: ")
+#     total= 25000*qu
+#     print(f"total amount:{total}")
+# elif option==3:
+#     qu= int(input("Enter quantity: "))
+#     name= input("Enter your name: ")
+#     total= 50000*qu
+#     print(f"total amount:{total}")
+# elif option==4:
+#     qu= int(input("Enter quantity: "))
+#     name= input("Enter your name: ")
+#     total= 30000*qu
+#     print(f"total amount:{total}")
+# else:
+#     print("Invalid option")
+  
+
+print("=================welcome to ATM========")
+pin =int(input("Enter your pin: "))
+if pin==1234:
+    amount=10000
+    print("1. withdraw  2. Balance Enquiry ")
+    option =int(input("select your option: "))
+    if option==1:
+        namount=int(input("Enter amount to withdraw: "))
+        if namount>amount:
+            print("insufficiemt balance")
+        else:
+            wamount= amount- namount
+            print(f"please collect your cash")
+            print(f"withdraw amount is :{namount}")
+            print(f"your remaining bakance is  :{wamount}")
+    elif option==2:
+        print(f"your balance is : {amount}")
+    else:
+        print("Invalid option")
 else:
-    print("Invalid option")
+    print("Incorrect pin")
+    
